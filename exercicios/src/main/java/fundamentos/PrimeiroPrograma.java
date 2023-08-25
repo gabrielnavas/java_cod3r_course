467eb194d08d2b27686d6b9c90fbfcb825f1968a
navas <gabrielnavas@email.com>
2023-08-24 23:36:51 -0300

    comentarios de uma única linha, múltiplas linhas, e java docs

--- exercicios/src/main/java/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/main/java/fundamentos/PrimeiroPrograma.java
@@ -1,7 +1,27 @@
 package fundamentos;
 
+/**
+ * This class represent a first programming in Java
+ *
+ * @author navas
+ * @since JDK1.0
+ * @see https://wwww.github.com/gabrielnavas
+ */
 public class PrimeiroPrograma {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
+
+        // A code sentence ends with a semicolon ;
+        // Again, another comment line
+
+        /*
+        * or use this
+        * for any line
+        * you want
+        * */
+
         System.out.println("Primeiro Programa! ;)");
     }
 }
