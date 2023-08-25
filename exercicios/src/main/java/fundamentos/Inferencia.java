d41d4bfb3d5ab55139373cbfc35d17b82f80b584
navas <gabrielnavas@email.com>
2023-08-25 01:10:59 -0300

    inferencia de variável usando var ou não

---
+++ exercicios/src/main/java/fundamentos/Inferencia.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class Inferencia {
+    public static void main(String[] args) {
+        double a = 4.5;
+        System.out.println(a);
+
+        var b = 6.5;
+        System.out.println(b);
+
+        var c = "texto";
+        System.out.println(c);
+
+        double d;
+        d = 8.5;
+        System.out.println(d);
+    }
+}
