16ca480f37b926a9d602c4637beb46ef00aa79ee
navas <gabrielnavas@email.com>
2023-08-26 17:55:39 -0300

    jre vs jdk

---
+++ exercicios/src/main/java/fundamentos/JREvsJDK.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+public class JREvsJDK {
+    public static void main(String[] args) {
+        /*
+        * JRE.
+        * utilizado para executar um codigo compilado em java, que são os .class.
+        * JRE quer dizer Java Runtime Env.
+        *
+        * JDK
+        * É usado para o ambiente de desenvolvimento, compilando os arquivos .java, que são os bytecode.
+        * JDK quer dizer Java development Kit.
+        * */
+
+
+        /*
+        * JVM
+        * Executar o mesmo código em diferentes hardwares, que pode ser PC, Apple, Android ou uma geladeira por exemplo.
+        * A JVM pega o bytecode compilado polo JDK e executa, independentemente do tipo de hardware.
+        * O única requisito é ter o JRE para o hardware correto;
+        *
+        * */
+    }
+}
