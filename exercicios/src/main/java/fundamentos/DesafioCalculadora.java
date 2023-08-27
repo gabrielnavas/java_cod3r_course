92d3b1ffb437fa21e3ad441380e2aa80079df9e1
navas <gabrielnavas@email.com>
2023-08-27 01:30:58 -0300

    desafio calculadora

--- exercicios/src/main/java/fundamentos/DesafioCalculadora.java
+++ exercicios/src/main/java/fundamentos/DesafioCalculadora.java
@@ -1,2 +1,30 @@
-package fundamentos;public class DesafioCalculadora {
+package fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioCalculadora {
+    public static void main(String[] args) {
+        // sem if ou switch
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double num1 = Double.valueOf(entrada.nextLine().trim());
+
+        System.out.print("Digite o segundo número: ");
+        double num2 = Double.valueOf(entrada.nextLine().trim());
+
+        System.out.print("Digite a operação desejada: [+ - * / %]");
+        String operacao = entrada.nextLine().trim();
+
+        double resultado = "+".equals(operacao) ? num1 + num2 : 0.00;
+        resultado = "-".equals(operacao) ? num1 - num2 : resultado;
+        resultado = "*".equals(operacao) ? num1 * num2 : resultado;
+        resultado = "/".equals(operacao) ? num1 / num2 : resultado;
+        resultado = "%".equals(operacao) ? num1 % num2 : resultado;
+
+        System.out.printf("Resultado foi %.2f %s %.2f = %.2f.", num1, operacao, num2, resultado);
+
+        entrada.close();
+    }
 }
