7e18c6dbf1dc1af555c5fac3057068056a994714
navas <gabrielnavas@email.com>
2023-08-27 15:00:15 -0300

    método com data formatada podendo ser formato americano ou padrão pt-br

--- exercicios/src/main/java/classe/MinhaData.java
+++ exercicios/src/main/java/classe/MinhaData.java
@@ -4,4 +4,42 @@ public class MinhaData {
     int dia;
     int mes;
     int ano;
+
+
+    String obterDataFormatada(boolean formatoAmericano) {
+        String dataResultante;
+
+
+        String diaFormatado;
+        String mesFormatado;
+        String anoFormatado;
+
+        if (dia < 10) {
+            diaFormatado = String.format("0%d", dia);
+        } else {
+            diaFormatado = String.format("%d", dia);
+        }
+        if (mes < 10) {
+            mesFormatado = String.format("0%d", mes);
+        } else {
+            mesFormatado = String.format("%d", mes);
+        }
+        if (ano < 1000) {
+            anoFormatado = String.format("0%d", ano);
+        } else if (ano < 100) {
+            anoFormatado = String.format("00%d", ano);
+        } else if (ano < 10) {
+            anoFormatado = String.format("000%d", ano);
+        } else {
+            anoFormatado = String.format("%d", ano);
+        }
+
+        if (formatoAmericano) {
+            dataResultante = String.format("%s/%s/%s", mesFormatado, diaFormatado, anoFormatado);
+        } else {
+            dataResultante = String.format("%s/%s/%s", diaFormatado, mesFormatado, anoFormatado);
+        }
+
+        return dataResultante;
+    }
 }
--- exercicios/src/main/java/classe/MinhaDataTeste.java
+++ exercicios/src/main/java/classe/MinhaDataTeste.java
@@ -5,16 +5,19 @@ import java.util.Date;
 public class MinhaDataTeste {
     public static void main(String[] args) {
         MinhaData md1 =new MinhaData();
-        md1.dia = 25;
+        md1.dia = 1;
         md1.mes = 12;
         md1.ano = 2008;
 
         MinhaData md2 =new MinhaData();
         md2.dia = 22;
-        md2.mes = 06;
-        md2.ano = 1999;
+        md2.mes = 6;
+        md2.ano = 999;
 
-        System.out.printf("%s/%s/%s\n", md1.dia, md1.mes, md1.ano);
-        System.out.printf("%s/%s/%s\n", md2.dia, md2.mes, md2.ano);
+        System.out.println(md1.obterDataFormatada(true));
+        System.out.println(md1.obterDataFormatada(false));
+
+        System.out.println(md2.obterDataFormatada(true));
+        System.out.println(md2.obterDataFormatada(false));
     }
 }
