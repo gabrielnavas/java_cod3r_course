587c120711e7822738e99361a4266e404d9ceee9
navas <gabrielnavas@email.com>
2023-08-28 00:57:40 -0300

    for each

---
+++ exercicios/src/main/java/arrays/Foreach.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class Foreach {
+    public static void main(String[] args) {
+        String[] nomes = {"Gabriel", "João", "Maria"};
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+    }
+}
