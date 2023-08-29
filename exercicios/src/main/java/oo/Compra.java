ccb86282fdda34a467f054992a1200fc1ac18ff4
navas <gabrielnavas@email.com>
2023-08-29 01:22:10 -0300

    orientação a objetos mostrando relacionamento 1 para N

---
+++ exercicios/src/main/java/oo/Compra.java
@@ -0,0 +1,33 @@
+package oo;
+
+import java.util.ArrayList;
+
+public class Compra {
+
+    String nomeCliente;
+    ArrayList<Item> itens;
+
+    Compra(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+        itens = new ArrayList<>();
+    }
+
+    void adicionarItem(Item item) {
+        item.compra = this;
+        itens.add(item);
+    }
+
+    void adicionarItem(String nome, int quantidade,double preco) {
+        Item item = new Item(nome, quantidade, preco);
+        item.compra = this;
+        itens.add(item);
+    }
+
+    double obterValorTotal() {
+        double total = 0.0;
+        for (Item item : itens) {
+            total += item.preco * item.quantidade;
+        }
+        return total;
+    }
+}
---
+++ exercicios/src/main/java/oo/CompraTeste.java
@@ -0,0 +1,16 @@
+package oo;
+
+public class CompraTeste {
+    public static void main(String[] args) {
+        Compra c1  = new Compra("João");
+        c1.adicionarItem(new Item("Apple", 10, 5000.00));
+        c1.adicionarItem(new Item("HDD", 20, 200.00));
+        c1.adicionarItem("Mouse", 30, 100.00);
+
+        System.out.println(c1.itens.size());
+        System.out.println(c1.obterValorTotal());
+
+        // um para um, relação bidirecional
+        System.out.println(c1.itens.get(0).compra.itens.get(0).nome);
+    }
+}
