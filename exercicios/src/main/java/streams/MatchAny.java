3dc62e2464920370444a72b4ee937dfab582fa29
navas <gabrielnavas@email.com>
2023-09-04 20:43:01 -0300

    usando negate disponível na interface predicate

--- exercicios/src/main/java/streams/MatchAny.java
+++ exercicios/src/main/java/streams/MatchAny.java
@@ -10,7 +10,13 @@ public class MatchAny {
 
         Predicate<Double> aprovado = n -> n > 7.0;
 
+
+        // pega se tem algum true
         Boolean algumAlunoAprovado = notas.stream().anyMatch(aprovado);
+        System.out.printf("algum aluno aprovado? %b\n", algumAlunoAprovado);
+
+        // da pra usar o negate, pegando se tem algum false
+        Boolean algumAlunoReprovado = notas.stream().anyMatch(aprovado.negate());
         System.out.printf("algum aluno aprovado? %b\n", algumAlunoAprovado);
     }
 }
