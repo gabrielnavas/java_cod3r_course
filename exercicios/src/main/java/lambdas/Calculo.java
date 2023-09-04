2336de61b2159ff4c7efdf40203e710eb5ffaa50
navas <gabrielnavas@email.com>
2023-09-03 21:23:41 -0300

    usando métodos com implementação default ou static dentro de interfaces com anotação FunctionalInterface

--- exercicios/src/main/java/lambdas/Calculo.java
+++ exercicios/src/main/java/lambdas/Calculo.java
@@ -3,4 +3,12 @@ package lambdas;
 @FunctionalInterface
 public interface Calculo {
     double executar(double a, double b);
+
+    default String metodoComImplementacaoPode() {
+        return "massa!";
+    }
+
+    static String metodoStaticPodeTambem() {
+        return "sou estatico";
+    }
 }
--- exercicios/src/main/java/lambdas/CalculoTesteFunctional.java
+++ exercicios/src/main/java/lambdas/CalculoTesteFunctional.java
@@ -7,5 +7,9 @@ public class CalculoTesteFunctional {
 
         calculo = (x, y) -> x * y;
         System.out.println(calculo.executar(2, 3));
+
+        System.out.println(calculo.metodoComImplementacaoPode());
+
+        System.out.println(Calculo.metodoStaticPodeTambem());
     }
 }
