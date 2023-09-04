8c7ae7f24f5f511540cdeb3d9ff3200779a5dbd2
navas <gabrielnavas@email.com>
2023-09-04 00:31:54 -0300

    usando for each, usando uma função static

--- exercicios/src/main/java/lambdas/Foreach.java
+++ exercicios/src/main/java/lambdas/Foreach.java
@@ -10,7 +10,14 @@ public class Foreach {
         // passando explicitamente o aprovado
         aprovados.forEach(aprovado -> System.out.println(aprovado));
 
-        // passadno implicitamente o aprovado
+        // passando implicitamente o aprovado
         aprovados.forEach(System.out::println);
+
+        // passando implicitamente uma outra função
+        aprovados.forEach(Foreach::imprimir);
+    }
+
+    private static void imprimir(String str) {
+        System.out.println(str);
     }
 }
