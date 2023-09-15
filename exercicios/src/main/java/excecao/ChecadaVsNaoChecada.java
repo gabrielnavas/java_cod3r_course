ab414f1aee9c3b332b37606e1ed83bfc19077b7b
navas <gabrielnavas@email.com>
2023-09-15 17:36:58 -0300

    diferenciando exceptions e errors checados e não checados.

--- exercicios/src/main/java/excecao/ChecadaVsNaoChecada.java
+++ exercicios/src/main/java/excecao/ChecadaVsNaoChecada.java
@@ -2,16 +2,53 @@ package excecao;
 
 public class ChecadaVsNaoChecada {
     public static void main(String[] args) {
-        gerarError1();
-        gerarError2();
+
+        // quando lançado uma exception checada, é necessário tratar
+        try {
+            gerarException();
+        } catch (Exception e) {
+            System.out.println("houve uma exception genérica");
+        }
+
+        // exceptions ou errors não checados não precisam ser tratados.
+        gerarRunetimeException();
+        gerarOutOfMemoryError();
+        gerarError();
+
+        try {
+            gerarRunetimeException();
+            gerarOutOfMemoryError();
+            gerarError();
+        }
+        catch (RuntimeException ex) {
+            System.out.println("houve uma exception do tipo Exception");
+        }
+        catch (OutOfMemoryError ex) {
+            System.out.println("houve um error do tipo OutOfMemoryError");
+        }
+        catch (VirtualMachineError ex) {
+            System.out.println("houve um error do tipo VirtualMachineError");
+        }
+        catch (Error ex) {
+            System.out.println("houve um error do tipo Error");
+        }
     }
 
-    static void gerarError1() {
-        new RuntimeException("Ocorreu um error bem legal #01!");
+    // é preciso deixar claro que tem uma exception checada.
+    static void gerarException() throws Exception {
+        throw new Exception("Ocorreu um error bem legal #01!");
     }
 
-    static void gerarError2() {
-        new Exception("Ocorreu um error bem legal #01!");
+    static void gerarRunetimeException() {
+        throw new RuntimeException("Ocorreu um error bem legal #01!");
+    }
+
+    static void gerarOutOfMemoryError() {
+        throw new OutOfMemoryError("Ocorreu um error bem legal #01!");
+    }
+
+    static void gerarError() {
+        throw new Error("Ocorreu um error bem legal #01!");
     }
 
 }
