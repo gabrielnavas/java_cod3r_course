f1c93228bd2db1ee572bd58092ec3a9af51db7cf
navas <gabrielnavas@email.com>
2023-09-15 22:46:50 -0300

    init project campo minado

---
+++ campominado/src/test/java/com/navas/campominado/CampominadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navas.campominado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampominadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
