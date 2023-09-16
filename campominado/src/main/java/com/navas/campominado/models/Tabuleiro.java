6025ec3e450dc292b260013e1d6ce50948016534
navas <gabrielnavas@email.com>
2023-09-16 18:26:54 -0300

    implementado gerar campos, associar os vizinhos e get campos. Testes adicionados ao método Tabuleiro.gerarCampos.

--- campominado/src/main/java/com/navas/campominado/models/Tabuleiro.java
+++ campominado/src/main/java/com/navas/campominado/models/Tabuleiro.java
@@ -1,4 +1,53 @@
 package com.navas.campominado.models;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
 public class Tabuleiro {
+
+    private final List<Campo> campos = new ArrayList<>();
+    private int linhas;
+    private int colunas;
+    private int minas;
+
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+
+        gerarCampos();
+        associarOsVizinhos();
+        sortearMinas();
+    }
+
+    private void gerarCampos() {
+        IntStream.range(0, linhas).forEach(linha ->
+                IntStream.range(0, colunas).forEach(coluna -> {
+                    Campo campo = new Campo(linha, coluna);
+                    campos.add(campo);
+                }));
+    }
+
+    private void associarOsVizinhos() {
+        campos.forEach(campoA -> campos.forEach(campoA::adicionarVizinho));
+    }
+
+    private void sortearMinas() {
+    }
+
+    public Campo[][] getCampos() {
+        Campo[][] campos = new Campo[linhas][colunas];
+        IntStream.range(0, linhas).forEach(linha ->
+                IntStream.range(0, colunas).forEach(coluna -> {
+                    Optional<Campo> campo = this.campos
+                            .stream()
+                            .filter(campoN -> campoN.getLinha() == linha
+                                    && campoN.getColuna() == coluna
+                            ).findFirst();
+                    campo.ifPresent(value -> campos[linha][coluna] = value);
+                }));
+        return campos;
+    }
 }
