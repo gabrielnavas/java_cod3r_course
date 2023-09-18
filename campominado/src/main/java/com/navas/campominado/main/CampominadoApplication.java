2c666d7411dc5a38750d254a1d7da5b45303c621
navas <gabrielnavas@email.com>
2023-09-18 02:00:39 -0300

    movido o main do spring para pacote main

--- campominado/src/main/java/com/navas/campominado/main/CampominadoApplication.java
+++ campominado/src/main/java/com/navas/campominado/main/CampominadoApplication.java
@@ -1,4 +1,4 @@
-package com.navas.campominado;
+package com.navas.campominado.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CampominadoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CampominadoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CampominadoApplication.class, args);
+    }
 
 }
--- campominado/src/test/java/com/navas/campominado/main/CampominadoApplicationTests.java
+++ campominado/src/test/java/com/navas/campominado/main/CampominadoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.navas.campominado;
+package com.navas.campominado.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CampominadoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
