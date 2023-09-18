0e85fe3c83b56590c70744c3bf95dfc5d6bbf301
navas <gabrielnavas@email.com>
2023-09-18 20:53:15 -0300

    adicionando diversas pessoas como observadores para o evento

--- exercicios/src/main/java/padroes/observer/AniversarioSurpresa.java
+++ exercicios/src/main/java/padroes/observer/AniversarioSurpresa.java
@@ -4,9 +4,10 @@ public class AniversarioSurpresa {
     public static void main(String[] args) {
         SubjectPorteiro porteiro = new SubjectPorteiro();
 
-        porteiro.registrarObservador(evento -> {
-            System.out.printf("evento iniciou as %s", evento);
-        });
+        porteiro.registrarObservador(evento -> System.out.printf("1º pessoa do evento. Evento iniciou as %s\n", evento));
+        porteiro.registrarObservador(evento -> System.out.printf("2º pessoa do evento. Evento iniciou as %s\n", evento));
+        porteiro.registrarObservador(evento -> System.out.printf("3º pessoa do evento. Evento iniciou as %s\n", evento));
+        porteiro.registrarObservador(evento -> System.out.printf("4º pessoa do evento. Evento iniciou as %s\n", evento));
 
         porteiro.monitorar();
     }
