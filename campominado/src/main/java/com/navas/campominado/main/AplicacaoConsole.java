97331975b285c68f9acbd36c631690b25d96fc55
navas <gabrielnavas@email.com>
2023-09-18 16:32:25 -0300

    corrigido teste toString.

--- campominado/src/main/java/com/navas/campominado/main/AplicacaoConsole.java
+++ campominado/src/main/java/com/navas/campominado/main/AplicacaoConsole.java
@@ -6,6 +6,6 @@ import com.navas.campominado.views.TabuleiroConsole;
 public class AplicacaoConsole {
     public static void main(String[] args) {
         Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-        TabuleiroConsole tabuleiroConsole = new TabuleiroConsole(tabuleiro);
+        new TabuleiroConsole(tabuleiro);
     }
 }
