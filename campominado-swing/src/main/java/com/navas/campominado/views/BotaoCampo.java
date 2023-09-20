ccae492ad50ba56fbc83e20c767df97b72a65033
navas <gabrielnavas@email.com>
2023-09-20 01:59:56 -0300

    adicionado a classe BotaoCampo

---
+++ campominado-swing/src/main/java/com/navas/campominado/views/BotaoCampo.java
@@ -0,0 +1,12 @@
+package com.navas.campominado.views;
+
+import com.navas.campominado.models.Campo;
+
+import javax.swing.*;
+
+public class BotaoCampo extends JButton {
+
+    public BotaoCampo(Campo campo) {
+    }
+
+}
--- campominado-swing/src/main/java/com/navas/campominado/views/PainelTabuleiro.java
+++ campominado-swing/src/main/java/com/navas/campominado/views/PainelTabuleiro.java
@@ -3,12 +3,16 @@ package com.navas.campominado.views;
 import com.navas.campominado.models.Tabuleiro;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PainelTabuleiro extends JPanel {
 
-    private final Tabuleiro tabuleiro;
-
     public PainelTabuleiro(Tabuleiro tabuleiro) {
-        this.tabuleiro = tabuleiro;
+        setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
+        tabuleiro.paraCadaCampo(campo -> add(new BotaoCampo(campo)));
+
+        tabuleiro.registrarObservador(e -> {
+            
+        });
     }
 }
